eb727bc1699ab3e7666724d93338fd5fde11628e
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-11-07 22:51:55 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,50 @@
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r', encoding='utf-8')
+        data = file.read()
+        print(data)
+        file.close()
+        #print(f'{data}')
+
+    def add(self, *products):
+        file = open(self.__file_name, 'a', encoding='utf-8')
+        file.write('')  # Создание файла при первом запуске и добавление "ничего" при последующих
+        file.close()
+        for i in products:
+            s = (str(i))
+            file = open(self.__file_name, 'r', encoding='utf-8')
+            f = file.read()
+            file.close()
+            if s in f:
+                print(f'Продукт {s} уже есть в магазине')
+            else:
+                file = open(self.__file_name, 'a', encoding='utf-8')
+                file.write(f'\n{s}')
+                file.close()
+
+
+##########################################
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
