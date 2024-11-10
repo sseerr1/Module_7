c75d6212ef30556eed4e2d2fdf20a0fba8c3df64
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2024-11-10 21:41:02 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,23 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    file = open(file_name, 'w', encoding='utf-8')
+    for string in strings:
+        position = file.tell()
+        file.write(string + '\n')
+        strings_positions[(len(strings_positions) + 1, position)] = string
+    file.close()
+
+    return strings_positions
+############################################################
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
